4632df599fd6a9d75cbd837c63e4c80b09fb29ee
ltetrel <loic.tetrel.pro@gmail.com>
2021-02-22 14:19:26 -0500

    keyboard events should be working

--- fmriprep-qc/main.py
+++ fmriprep-qc/main.py
@@ -86,7 +86,7 @@ def build_app(derivatives_path):
     app.layout = html.Div(
         [
             Keyboard(id="keyboard"),
-            dcc.Store(id='idx_fname', data=idx_fname),
+            dcc.Store(id='fname-idx', data=0),
             dcc.Store(id='left_press', data=False),
             dcc.Store(id='right_press', data=False),
             dcc.Dropdown(
@@ -129,7 +129,6 @@ def build_app(derivatives_path):
     )
     def update_left(key_status, n_press, data):
         if key_status:
-            # if (key_status["key"] == "ArrowLeft") & (key_status["shiftKey"] == True):
             if (key_status["key"] == "ArrowLeft"):
                 return True
         return False
@@ -144,50 +143,53 @@ def build_app(derivatives_path):
     )
     def update_right(key_status, n_press, data):
         if key_status:
-            # if (key_status["key"] == "ArrowRight") & (key_status["shiftKey"] == True):
             if (key_status["key"] == "ArrowRight"):
                 return True
         return False
 
     @app.callback(
-        dash.dependencies.Output('idx_fname', 'data'),
+        dash.dependencies.Output("fname-idx", "data"),
         [
-            dash.dependencies.Input("subject-dropdown", "value"),
+            dash.dependencies.Input("run-dropdown", "value"),
+            dash.dependencies.Input("run-dropdown", "options"),
             dash.dependencies.Input("keyboard", "n_keydowns"),
         ],
         [
-            dash.dependencies.State('idx_fname', 'data'),
+            dash.dependencies.State("fname-idx", "data"),
             dash.dependencies.State('left_press', 'data'),
             dash.dependencies.State('right_press', 'data'),
         ]
     )
-    def update_idx_fname(subject, n_press, idx_fname, left_press, right_press):
-        fnames = list_runs(subject)[1]
-
+    def update_fname_idx(fname, fnames, n_press, fname_idx, left_press, right_press):
+        for ii, curr_fname in enumerate(fnames):
+            if curr_fname["value"] == fname:
+                fname_idx = ii
         if right_press:
-            if idx_fname < (len(fnames) - 1):
-                idx_fname = idx_fname + 1
+            if fname_idx < (len(fnames) - 1):
+                fname_idx = fname_idx + 1
         elif left_press:
-            if idx_fname > 0:
-                idx_fname = idx_fname - 1
+            if fname_idx > 0:
+                fname_idx = fname_idx - 1
                     
-        return idx_fname
+        return fname_idx
+
 
     @app.callback(
         dash.dependencies.Output("run-dropdown", "value"),
         [
             dash.dependencies.Input("subject-dropdown", "value"),
+            dash.dependencies.Input("run-dropdown", "options"),
             dash.dependencies.Input("keyboard", "n_keydowns"),
         ],
         [
-            dash.dependencies.State('idx_fname', 'data'),
+            dash.dependencies.State("fname-idx", "data"),
             dash.dependencies.State('left_press', 'data'),
             dash.dependencies.State('right_press', 'data'),
         ]
     )
-    def update_runs_value(subject, n_press, idx_fname, left_press, right_press):
+    def update_run_value(subject, fnames, n_press, fname_idx, left_press, right_press):
         if left_press | right_press:
-            return list_runs(subject)[1][idx_fname]
+            return list_runs(subject)[1][fname_idx]
         return list_runs(subject)[1][0]
 
     @app.callback(
@@ -196,29 +198,13 @@ def build_app(derivatives_path):
             dash.dependencies.Input("subject-dropdown", "value"),
             dash.dependencies.Input("run-dropdown", "value"),
             dash.dependencies.Input("step-tabs", "value"),
-            dash.dependencies.Input("run-dropdown", "options"),
         ],
-        [
-            dash.dependencies.State('idx_fname', 'data'),
-            dash.dependencies.State('left_press', 'data'),
-            dash.dependencies.State('right_press', 'data'),
-        ]
     )
-    def update_image_src(subject, fname, step, fnames, idx_fname, left_press, right_press):
-        # print("####################")
-        # print([key_status, n_press])
-        print("{} idx - {} - left_press {} - right_press {} \n".format("update_idx_fname", idx_fname, left_press, right_press))
-
-        #https://stackoverflow.com/questions/62731812/how-do-you-store-variables-in-dash-core-components
+    def update_image_src(subject, fname, step):
         if fname:
-            if (left_press | right_press):
-                return os.path.join(
-                            static_image_route, subject, fnames[idx_fname]["value"].replace(f"-{default_preproc_step}_", "-{}_".format(step))
+            return os.path.join(
+                    static_image_route, subject, fname.replace(f"-{default_preproc_step}_", "-{}_".format(step))
                 )
-            else:
-                return os.path.join(
-                        static_image_route, subject, fname.replace(f"-{default_preproc_step}_", "-{}_".format(step))
-                    )
 
     @app.server.route("/images/<subject>/<image_path>")
     def serve_image(subject, image_path):
